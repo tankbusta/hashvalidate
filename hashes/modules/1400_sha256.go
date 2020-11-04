d8b32c63c7c8d3ec3b3d3bfd95ccec3a88b19044
Christopher Schmitt <cschmitt@tankbusta.net>
2020-11-04 14:13:56 -0700

    feat: add sha1 and sha256 hashes

---
+++ hashes/modules/1400_sha256.go
@@ -0,0 +1,30 @@
+package modules
+
+import (
+	"github.com/tankbusta/hashvalidate/hashes"
+	"github.com/tankbusta/hashvalidate/tokenizer"
+)
+
+type sha256_1400 struct{}
+
+func init() {
+	hashes.Register(1400, sha256_1400{})
+}
+
+func (s sha256_1400) Name() string { return "SHA-256" }
+
+func (s sha256_1400) Example() string {
+	return "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935"
+}
+
+func (s sha256_1400) Type() int { return 1400 }
+
+func (s sha256_1400) Tokens() []tokenizer.Token {
+	return []tokenizer.Token{
+		{
+			LengthMin:  40,
+			LengthMax:  40,
+			Attributes: tokenizer.VerifyLength | tokenizer.VerifyHex,
+		},
+	}
+}
